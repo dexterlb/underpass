f141db59cd0542c15cb94a53ea6b3fdf75dd157f
dexterlb <dexterlb@qtrp.org>
2019-01-07 15:50:38 +0200

    topo sort for resolver

--- utils/src/Utils/Resolver.hs
+++ utils/src/Utils/Resolver.hs
@@ -8,12 +8,14 @@ module Utils.Resolver where
 -- import qualified Data.HashMap.Lazy as HM
 import           Data.HashMap.Lazy (HashMap)
 import qualified Data.HashMap.Lazy as HM
+import qualified Data.HashSet as HS
 import           Data.HashSet (HashSet)
 import           Data.Hashable (Hashable)
+import qualified Data.Graph as G
 
 type Library a = HashMap (ResolveKey a) (Resolved a)
 
-class (Eq (ResolveKey a), Hashable (ResolveKey a)) => Resolvable a where
+class (Eq (ResolveKey a), Ord (ResolveKey a), Hashable (ResolveKey a)) => Resolvable a where
     type ResolveKey a
     type Resolvee a
     type Resolved a
@@ -25,7 +27,7 @@ class (Eq (ResolveKey a), Hashable (ResolveKey a)) => Resolvable a where
 resolveLibrary :: (Resolvable a) => a -> HashMap (ResolveKey a) (Resolvee a) -> Library a
 resolveLibrary r plib = foldr (addToLibrary r) (emptyLib r) $ map (\k -> (k, plib HM.! k)) keys
     where
-        keys = topoSort $ map (\(k, v) -> (k, fv r v)) $ HM.toList plib
+        keys = topoSort $ map (\(k, v) -> (k, HS.toList $ fv r v)) $ HM.toList plib
 
 addToLibrary :: (Resolvable a) => a -> (ResolveKey a, Resolvee a) -> Library a -> Library a
 addToLibrary r (k, v) lib = HM.insert k (resolveItem r lib v) lib
@@ -42,5 +44,9 @@ emptyLib _ = HM.empty
 mergeLib :: (Resolvable a) => a -> Library a -> Library a -> Library a
 mergeLib _ = HM.union
 
-topoSort :: (Eq a) => [(a, HashSet a)] -> [a]
-topoSort = undefined
+topoSort :: (Eq a, Ord a) => [(a, [a])] -> [a]
+topoSort l = map (vertex . index) $ G.topSort graph
+    where
+        vertex ((), k, _) = k
+        (graph, index, _) = G.graphFromEdges $ map (\(k, e) -> ((), k, e)) l
+
