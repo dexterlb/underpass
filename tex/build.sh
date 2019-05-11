12eb296064d421587c5883e0433a2185f93191f7
dexterlb <dexterlb@qtrp.org>
2019-05-11 16:04:28 +0300

    externalise figures

--- tex/build.sh
+++ tex/build.sh
@@ -6,11 +6,11 @@ cd "$(dirname "$(readlink -f "${0}")")"
 
 case "${1}" in
     "")
-        latexmk -halt-on-error -xelatex main.tex
+        latexmk -latexoption="-shell-escape" -halt-on-error -xelatex main.tex
         ;;
     clean)
         latexmk -C
-        for f in *.bbl *.run.xml; do
+        for f in *.bbl *.run.xml *.auxlock *-figure*.{pdf,log,dpth,md5}; do
             rm -rvf "${f}"
         done
         ;;
